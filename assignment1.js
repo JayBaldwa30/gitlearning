6d7f4727589da0a31e9158523aa1849fe40c38a1
jaybaldwa30 <jaybaldwa123@gmail.com>
2022-12-01 11:52:21 +0530

    uploadiong asssignments 1,2,3 on feature

---
+++ assignment1.js
@@ -0,0 +1,22 @@
+// assignment 1
+
+// variable declaration 
+
+// const name = "jay";
+// const email = "jaybaldwa123@gmail.com";
+// const age = 20;
+// const address = '106 hb sch 94';
+// const phoneNumber = 7974749174;
+// const gender = "male";
+// const married = true ;
+
+
+// // printing the values 
+
+// console.log("My name is :-", name);
+// console.log("My email address is :-", email);
+// console.log("My current age is :-", age);
+// console.log("My residential address is :-", address);
+// console.log("My personal number is :-", phoneNumber);
+// console.log("My gender is :-", gender);
+// console.log("are you married  :-", married);
