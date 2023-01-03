9c3ee59474b0ec703a5a6966221ae065e17f743f
jaybaldwa30 <jaybaldwa123@gmail.com>
2023-01-03 11:59:02 +0530

    uploading assignments

--- assignment1.js
+++ assignment1.js
@@ -1,22 +1,27 @@
 // assignment 1
 
+/*Question:- Create a js file which contains variable declaration
+for a from which asks following basic details:
+[name, email, address, phone number,gender, married or not]*/
+
+// In this question, we are having the basic information for a form.
 // variable declaration 
 
-// const name = "jay";
-// const email = "jaybaldwa123@gmail.com";
-// const age = 20;
-// const address = '106 hb sch 94';
-// const phoneNumber = 7974749174;
-// const gender = "male";
-// const married = true ;
+const name = "jay";
+const email = "jaybaldwa123@gmail.com";
+const age = 20;
+const address = '106 hb sch 94';
+const phoneNumber = 7974749174;
+const gender = "male";
+const married = true ;
 
 
-// // printing the values 
+// printing the values 
 
-// console.log("My name is :-", name);
-// console.log("My email address is :-", email);
-// console.log("My current age is :-", age);
-// console.log("My residential address is :-", address);
-// console.log("My personal number is :-", phoneNumber);
-// console.log("My gender is :-", gender);
-// console.log("are you married  :-", married);
+console.log("My name is :-", name);
+console.log("My email address is :-", email);
+console.log("My current age is :-", age);
+console.log("My residential address is :-", address);
+console.log("My personal number is :-", phoneNumber);
+console.log("My gender is :-", gender);
+console.log("are you married  :-", married); 
---
+++ assignment4.js
@@ -0,0 +1,48 @@
+//Assignment 4 
+
+//Performing arithmetic operations on two numbers 
+
+//Two numbers
+let a=5;
+let b=5;
+
+console.log("Arithmetic operations on a and b are:-");
+console.log("a+b= ", a+b);            //addition 
+console.log("a-b= ", a-b);            //subtraction
+console.log("a*b= ", a*b);            //multiplication
+console.log("a/b= ", a/b);            //division
+console.log("a%b= ", a%b);            //modulo
+console.log("a**b= ", a**b);          //exponentiation 
+
+
+//Two strings
+let string1=`jay`;
+let string2=`baldwa`;
+
+//addition of two no.s and two strings in same sequence
+console.log(a,"+",b,"+",string1,"+",string2,"=",a+b+string1+string2)
+
+//addition of two strings and two no.s in same sequence
+console.log(string1,"+",string2,"+",a,"+",b,"=",string1+string2+a+b)
+
+
+//operation to get the result as NaN
+console.log(string1/0);
+
+
+//Two boolean variables
+let bool1=true;
+let bool2=false;
+
+// logical and and logical or operation on boolean variables respectively
+console.log(bool1,"&&",bool2,"=", bool1&&bool2);
+console.log(bool1,"||",bool2,"=", bool1||bool2);
+
+
+// demonstration of == and === operator.
+console.log(a,"==",string1,"=", a==string1);
+console.log(a,"===",string1,"=", a===string1);
+
+
+
+
